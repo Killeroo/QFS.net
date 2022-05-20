92c2b3ae798b103421edaaae7ed99176f471413e
noah-severyn <50927852+noah-severyn@users.noreply.github.com>
2022-05-19 20:58:52 -0400

    additional comments and rename CCs for clarity

--- QFS.cs
+++ QFS.cs
@@ -9,7 +9,7 @@ namespace QFS.net
     /// Note that this implementation contains control characters and other changes specific to SimCity 4.
     /// You can read about other game specifics at this specification for QFS spec http://wiki.niotso.org/RefPack.
     /// 
-    /// Ported from https://github.com/wouanagaine/SC4Mapper-2013/blob/db29c9bf88678a144dd1f9438e63b7a4b5e7f635/Modules/qfs.c#L25
+    /// Ported from https://github.com/wouanagaine/SC4Mapper-2013/blob/db29c9bf88678a144dd1f9438e63b7a4b5e7f635/Modules/qfs.ctrlByte4#L25
     /// 
     /// More information on file specification:
     /// - https://www.wiki.sc4devotion.com/index.php?title=DBPF_Compression
@@ -77,7 +77,7 @@ namespace QFS.net
         /// <param name="data">Compressed array of data</param>
         /// <returns>Uncompressed data array</returns>
         /// <example>
-        /// <c>
+        /// <ctrlByte4>
         /// // Load save game
         /// SC4SaveFile savegame = new SC4SaveFile(@"C:\Path\To\Save\Game.sc4");
         /// 
@@ -86,13 +86,19 @@ namespace QFS.net
         /// 
         /// // Decompress data (This file will normally be compressed, should ideally check before decompressing)
         /// byte[] decompressedData = QFS.UncompressData(data);
-        /// </c>
+        /// </ctrlByte4>
         /// </example>
         /// <exception cref="System.IndexOutOfRangeException">
         /// Thrown when the compression algorithm tries to access an element that is out of bounds in the array
         /// </exception>
         public static byte[] UncompressData(byte[] data)
         {
+            //If data is not compressed do not run it through the algorithm otherwise it will return junk
+            if (!IsCompressed(data)) {
+                return data;
+			}
+
+
             byte[] sourceBytes = data;
             byte[] destinationBytes;
             int sourcePosition = 0;
@@ -108,14 +114,8 @@ namespace QFS.net
                 header[i] = sourceBytes[i + 4];
             }
 
-            // First 2 bytes should be the QFS identifier
-            // Next 3 bytes should be the uncompressed size of file
-            // (we do this by byte shifting (from most significant byte to least))
-            // the last 3 bytes of the header to make a number)
-            uint uncompressedSize = Convert.ToUInt32((long)(header[2] << 16) + (header[3] << 8) + header[4]); ;
-
             // Create our destination array
-            destinationBytes = new byte[uncompressedSize];
+            destinationBytes = new byte[GetDecompressedSize(data)];
 
             // Next set our position in the file
             // (The if checks if the first 4 bytes are the size of the file
@@ -131,77 +131,86 @@ namespace QFS.net
             //}
 
             // Above code is redundant for SimCity 4 saves as the QFS compressed files all have the same header length
+            // Bytes 0-1 are header identifier, 2-4 are uncompressed size, 5-8 are unused by SC4
             // (Check was throwing off start position and caused decompression to get buggered)
             sourcePosition = 9;
 
-            // In QFS the control character tells us what type of decompression operation we are going to perform (there are 4)
+            // In QFS the control character (CC) tells us what type of decompression operation we are going to perform (there are 4)
             // Most involve using the bytes proceeding the control byte to determine the amount of data that should be copied from what
-            // offset. These bytes are labeled a, b and c. Some operations only use 1 proceeding byte, others can use 3
-            byte controlCharacter = 0;
-            byte a = 0;
-            byte b = 0;
-            byte c = 0;
+            // offset. The CC can be 1 to 4 bytes, determined from byte1 of the CC
+            byte ctrlByte1 = 0; //ctrlByte1
+            byte ctrlByte2 = 0; //ctrlByte2, set if required
+            byte ctrlByte3 = 0; //ctrlByte3, set if required
+            byte ctrlByte4 = 0; //ctrlByte4, set if required
             int length = 0;
             int offset = 0;
 
-            // Main decoding loop
-            // Keep decoding while sourcePosition is in source array and position isn't 0xFC?
+            // Main decoding loop. Keep decoding while sourcePosition is in source array and position isn't 0xFC?
             while ((sourcePosition < sourceBytes.Length) && (sourceBytes[sourcePosition] < 0xFC))
             {
                 // Read our packcode/control character
-                controlCharacter = sourceBytes[sourcePosition];
+                ctrlByte1 = sourceBytes[sourcePosition];
 
                 // Read bytes proceeding packcode
-                a = sourceBytes[sourcePosition + 1];
-                b = sourceBytes[sourcePosition + 2];
+                ctrlByte2 = sourceBytes[sourcePosition + 1];
+                ctrlByte3 = sourceBytes[sourcePosition + 2];
 
-                // Check which packcode type we are dealing with
-                if ((controlCharacter & 0x80) == 0)
+                // Control Characters 0 to 127 (2 byte length CC)
+                if (ctrlByte1 >= 0x00 && ctrlByte1 <= 0x7F)
+                //if ((ctrlByte1 & 0x80) == 0)
                 {
                     // First we copy from the source array to the destination array
-                    length = controlCharacter & 3;
+                    length = ctrlByte1 & 3;
                     LZCompliantCopy(ref sourceBytes, sourcePosition + 2, ref destinationBytes, destinationPosition, length);
 
                     // Then we copy characters already in the destination array to our current position in the destination array
                     sourcePosition += length + 2;
                     destinationPosition += length;
-                    length = ((controlCharacter & 0x1C) >> 2) + 3;
-                    offset = ((controlCharacter >> 5) << 8) + a + 1;
+                    length = ((ctrlByte1 & 0x1C) >> 2) + 3;
+                    offset = ((ctrlByte1 >> 5) << 8) + ctrlByte2 + 1;
                     LZCompliantCopy(ref destinationBytes, destinationPosition - offset, ref destinationBytes, destinationPosition, length);
 
                     destinationPosition += length;
                 }
-                else if ((controlCharacter & 0x40) == 0)
+
+                // Control Characters 128 to 191 (3 byte length CC)
+                else if (ctrlByte1 >= 0x80 && ctrlByte1 <= 0xBF)
+                //else if ((ctrlByte1 & 0x40) == 0)
                 {
-                    length = (a >> 6) & 3;
+                    length = (ctrlByte2 >> 6) & 3;
                     LZCompliantCopy(ref sourceBytes, sourcePosition + 3, ref destinationBytes, destinationPosition, length);
 
                     sourcePosition += length + 3;
                     destinationPosition += length;
-                    length = (controlCharacter & 0x3F) + 4;
-                    offset = (a & 0x3F) * 256 + b + 1;
+                    length = (ctrlByte1 & 0x3F) + 4;
+                    offset = (ctrlByte2 & 0x3F) * 256 + ctrlByte3 + 1;
                     LZCompliantCopy(ref destinationBytes, destinationPosition - offset, ref destinationBytes, destinationPosition, length);
 
                     destinationPosition += length;
                 }
-                else if ((controlCharacter & 0x20) == 0)
-                {
-                    c = sourceBytes[sourcePosition + 3];
 
-                    length = controlCharacter & 3;
+                // Control Characters 192 to 223 (4 byte length CC)
+                else if (ctrlByte1 >= 0xC0 && ctrlByte1 <= 0xDF)
+                //else if ((ctrlByte1 & 0x20) == 0)
+                {
+                    ctrlByte4 = sourceBytes[sourcePosition + 3];
+
+                    length = ctrlByte1 & 3;
                     LZCompliantCopy(ref sourceBytes, sourcePosition + 4, ref destinationBytes, destinationPosition, length);
 
                     sourcePosition += length + 4;
                     destinationPosition += length;
-                    length = ((controlCharacter >> 2) & 3) * 256 + c + 5;
-                    offset = ((controlCharacter & 0x10) << 12) + 256 * a + b + 1;
+                    length = ((ctrlByte1 >> 2) & 3) * 256 + ctrlByte4 + 5;
+                    offset = ((ctrlByte1 & 0x10) << 12) + 256 * ctrlByte2 + ctrlByte3 + 1;
                     LZCompliantCopy(ref destinationBytes, destinationPosition - offset, ref destinationBytes, destinationPosition, length);
 
                     destinationPosition += length;
                 }
-                else
+
+                // Control Characters 224 to 251 (1 byte length CC)
+                else 
                 {
-                    length = (controlCharacter & 0x1F) * 4 + 4;
+                    length = (ctrlByte1 & 0x1F) * 4 + 4;
                     LZCompliantCopy(ref sourceBytes, sourcePosition + 1, ref destinationBytes, destinationPosition, length);
 
                     sourcePosition += length + 1;
@@ -210,6 +219,7 @@ namespace QFS.net
             }
 
             // Add trailing bytes
+            // Control Characters 252 to 255 (1 byte length CC)
             if ((sourcePosition < sourceBytes.Length) && (destinationPosition < destinationBytes.Length))
             {
                 LZCompliantCopy(ref sourceBytes, sourcePosition + 1, ref destinationBytes, destinationPosition, sourceBytes[sourcePosition] & 3);
@@ -243,9 +253,9 @@ namespace QFS.net
         /// <param name="length">Amount of data to copy</param>
         /// <remarks>
         /// With QFS (LZ77) we require an LZ compatible copy method between arrays, what this means practically is that we need to copy
-        /// stuff one byte at a time from arrays. This is, because with LZ compatible algorithms, it is complete legal to copy over data that overruns
+        /// stuff one byte at ctrlByte2 time from arrays. This is, because with LZ compatible algorithms, it is complete legal to copy over data that overruns
         /// the currently filled position in the destination array. In other words it is more than likely the we will be asked to copy over data that hasn't
-        /// been copied yet. It's confusing, so we copy things one byte at a time.
+        /// been copied yet. It's confusing, so we copy things one byte at ctrlByte2 time.
         /// </remarks>
         /// <exception cref="System.IndexOutOfRangeException">
         /// Thrown when the copy method tries to access an element that is out of bounds in the array
