a03d4f38495ae49f07727bcd0b9f664422f960aa
noah-severyn <50927852+noah-severyn@users.noreply.github.com>
2023-01-26 22:02:50 -0500

    formatting

--- QFS.cs
+++ QFS.cs
@@ -2,8 +2,7 @@
 using System.Numerics;
 using Microsoft.VisualBasic;
 
-namespace QFS
-{
+namespace QFS {
 	/// <summary>
 	/// Implementation of QFS/RefPack/LZ77 decompression. This compression is used on larger entries inside saves
 	/// </summary>
@@ -17,12 +16,11 @@ namespace QFS
 	/// - https://www.wiki.sc4devotion.com/index.php?title=DBPF_Compression
 	/// - http://wiki.niotso.org/RefPack#Naming_notes
 	/// </remarks>
-	public class QFS
-    {
-        /// <summary>
-        /// All QFS compressed items will contain this signature in the file header.
-        /// </summary>
-        private const ushort QFS_Signature = 0xFB10;
+	public class QFS {
+		/// <summary>
+		/// All QFS compressed items will contain this signature in the file header.
+		/// </summary>
+		private const ushort QFS_Signature = 0xFB10;
 		/// <summary>
 		/// Minimum byte size that can be compressed.
 		/// </summary>
@@ -45,202 +43,188 @@ namespace QFS
 		/// </summary>
 		/// <param name="entryData">Data to check</param>
 		/// <returns>TRUE if data is compressed; FALSE otherwise</returns>
-		public static bool IsCompressed(byte[] entryData)
-        {
-            if (entryData.Length > 6)
-            {
-                if (BitConverter.ToUInt16(entryData, 4) == QFS_Signature)
-                {
-                    return true;
-                }
-            }
-            return false;
-        }
+		public static bool IsCompressed(byte[] entryData) {
+			if (entryData.Length > 6) {
+				if (BitConverter.ToUInt16(entryData, 4) == QFS_Signature) {
+					return true;
+				}
+			}
+			return false;
+		}
 
-        /// <summary>
-        /// Returns data's decompressed length in bytes.
-        /// </summary>
-        /// <param name="cData">Data to check</param>
-        /// <returns>Size of decompressed data. If data is not compressed, the raw size is returned.</returns>
-        public static uint GetDecompressedSize(byte[] cData)
-        {
-            if (IsCompressed(cData))
-            {
-                //First 4 bytes is always the size of header + compressed data
+		/// <summary>
+		/// Returns data's decompressed length in bytes.
+		/// </summary>
+		/// <param name="cData">Data to check</param>
+		/// <returns>Size of decompressed data. If data is not compressed, the raw size is returned.</returns>
+		public static uint GetDecompressedSize(byte[] cData) {
+			if (IsCompressed(cData)) {
+				//First 4 bytes is always the size of header + compressed data
 
-                // Read 5 byte header
-                byte[] header = new byte[5];
-                for (int idx = 0; idx < 5; idx++)
-                {
-                    header[idx] = cData[idx + 4];
-                }
+				// Read 5 byte header
+				byte[] header = new byte[5];
+				for (int idx = 0; idx < 5; idx++) {
+					header[idx] = cData[idx + 4];
+				}
 
-                // After QFS identifier, next 3 bytes are the decompressed size ... byte shift most significant byte to least
-                return Convert.ToUInt32((header[2] << 16) + (header[3] << 8) + header[4]);
-            }
-            else
-            {
-                return (uint)cData.Length;
-            }
-        }
+				// After QFS identifier, next 3 bytes are the decompressed size ... byte shift most significant byte to least
+				return Convert.ToUInt32((header[2] << 16) + (header[3] << 8) + header[4]);
+			} else {
+				return (uint) cData.Length;
+			}
+		}
 
-        /// <summary>
-        /// Decompress data compressed with QFS/RefPack compression.
-        /// </summary>
-        /// <param name="data">Compressed data array</param>
-        /// <returns>Decompressed data array</returns>
-        /// <example>
-        /// <c>
-        /// // Load save game
-        /// SC4SaveFile savegame = new SC4SaveFile(@"C:\Path\To\Save\Game.sc4");
-        ///
-        /// // Read raw data for Region View Subfile from save
-        /// byte[] data = sc4Save.LoadIndexEntryRaw(REGION_VIEW_SUBFILE_TGI);
-        ///
-        /// // Decompress data (This file will normally be compressed, should idealy check before decompressing)
-        /// byte[] decompressedData = QFS.UncompressData(data);
-        /// </c>
-        /// </example>
-        /// <exception cref="System.IndexOutOfRangeException">
-        /// Thrown when the compression algorithm tries to access an element that is out of bounds in the array
-        /// </exception>
-        public static byte[] Decompress(byte[] data)
-        {
-            byte[] sourceBytes = data;
-            byte[] destinationBytes;
-            int sourcePosition = 0;
-            int destinationPosition = 0;
+		/// <summary>
+		/// Decompress data compressed with QFS/RefPack compression.
+		/// </summary>
+		/// <param name="sourceBytes">Compressed data array</param>
+		/// <returns>Decompressed data array</returns>
+		/// <example>
+		/// <c>
+		/// // Load save game
+		/// SC4SaveFile savegame = new SC4SaveFile(@"C:\Path\To\Save\Game.sc4");
+		///
+		/// // Read raw data for Region View Subfile from save
+		/// byte[] data = sc4Save.LoadIndexEntryRaw(REGION_VIEW_SUBFILE_TGI);
+		///
+		/// // Decompress data (This file will normally be compressed, should idealy check before decompressing)
+		/// byte[] decompressedData = QFS.UncompressData(data);
+		/// </c>
+		/// </example>
+		/// <exception cref="System.IndexOutOfRangeException">
+		/// Thrown when the compression algorithm tries to access an element that is out of bounds in the array
+		/// </exception>
+		public static byte[] Decompress(byte[] sourceBytes) {
+			if (!IsCompressed(sourceBytes)) {
+				return sourceBytes;
+			}
 
-            // Check first 4 bytes (size of header + compressed data)
-            uint compressedSize = BitConverter.ToUInt32(sourceBytes, 0);
+			byte[] destinationBytes;
+			int destinationPosition = 0;
 
-            // Next read the 5 byte header
-            byte[] header = new byte[5];
-            for (int i = 0; i < 5; i++)
-            {
-                header[i] = sourceBytes[i + 4];
-            }
+			// Check first 4 bytes (size of header + compressed data)
+			uint compressedSize = BitConverter.ToUInt32(sourceBytes, 0);
 
-            // First 2 bytes should be the QFS identifier
-            // Next 3 bytes should be the uncompressed size of file
-            // (we do this by byte shifting (from most significant byte to least))
-            // the last 3 bytes of the header to make a number)
-            uint uncompressedSize = Convert.ToUInt32((long)(header[2] << 16) + (header[3] << 8) + header[4]); ;
+			// Next read the 5 byte header
+			byte[] header = new byte[5];
+			for (int i = 0; i < 5; i++) {
+				header[i] = sourceBytes[i + 4];
+			}
 
-            // Create our destination array
-            destinationBytes = new byte[uncompressedSize];
+			// First 2 bytes should be the QFS identifier
+			// Next 3 bytes should be the uncompressed size of file
+			// (we do this by byte shifting (from most significant byte to least))
+			// the last 3 bytes of the header to make a number)
+			uint uncompressedSize = Convert.ToUInt32((long) (header[2] << 16) + (header[3] << 8) + header[4]); ;
 
-            // Next set our position in the file
-            // (The if checks if the first 4 bytes are the size of the file
-            // if so our start position is 4 bytes + 5 byte header if not then our
-            // offset is just the header (5 bytes))
-            //if ((sourceBytes[0] & 0x01) != 0)
-            //{
-            //    sourcePosition = 9;//8;
-            //}
-            //else
-            //{
-            //    sourcePosition = 5;
-            //}
+			// Create our destination array
+			destinationBytes = new byte[uncompressedSize];
 
-            // Above code is redundant for SimCity 4 saves as the QFS compressed files all have the same header length
-            // (Check was throwing off start position and caused decompression to get buggered)
-            sourcePosition = 9;
+			// Next set our position in the file
+			// (The if checks if the first 4 bytes are the size of the file
+			// if so our start position is 4 bytes + 5 byte header if not then our
+			// offset is just the header (5 bytes))
+			//if ((sourceBytes[0] & 0x01) != 0)
+			//{
+			//    sourcePosition = 9;//8;
+			//}
+			//else
+			//{
+			//    sourcePosition = 5;
+			//}
 
-            // In QFS the control character tells us what type of decompression operation we are going to perform (there are 4)
-            // Most involve using the bytes proceeding the control byte to determine the amount of data that should be copied from what
-            // offset. These bytes are labled a, b and c. Some operations only use 1 proceeding byte, others can use 3
-            byte ctrlByte1;
-            byte ctrlByte2;
-            byte ctrlByte3;
-            byte ctrlByte4;
-            int length;
-            int offset;
+			// Above code is redundant for SimCity 4 saves as the QFS compressed files all have the same header length
+			// (Check was throwing off start position and caused decompression to get buggered)
+			int sourcePosition = 9;
 
-            // Main decoding loop. Keep decoding while sourcePosition is in source array and position isn't 0xFC
-            while ((sourcePosition < sourceBytes.Length) && (sourceBytes[sourcePosition] < 0xFC))
-            {
-                // Read our packcode/control character
-                ctrlByte1 = sourceBytes[sourcePosition];
+			// In QFS the control character tells us what type of decompression operation we are going to perform (there are 4)
+			// Most involve using the bytes proceeding the control byte to determine the amount of data that should be copied from what
+			// offset. These bytes are labled a, b and c. Some operations only use 1 proceeding byte, others can use 3
+			byte ctrlByte1;
+			byte ctrlByte2;
+			byte ctrlByte3;
+			byte ctrlByte4;
+			int length;
+			int offset;
 
-                // Read bytes proceeding packcode
-                ctrlByte2 = sourceBytes[sourcePosition + 1];
-                ctrlByte3 = sourceBytes[sourcePosition + 2];
+			// Main decoding loop. Keep decoding while sourcePosition is in source array and position isn't 0xFC
+			while ((sourcePosition < sourceBytes.Length) && (sourceBytes[sourcePosition] < 0xFC)) {
+				// Read our packcode/control character
+				ctrlByte1 = sourceBytes[sourcePosition];
+
+				// Read bytes proceeding packcode
+				ctrlByte2 = sourceBytes[sourcePosition + 1];
+				ctrlByte3 = sourceBytes[sourcePosition + 2];
 
 				// Check which packcode type we are dealing with
 				// Control Characters 0 to 127 (2 byte length CC)
-				if ((ctrlByte1 & 0x80) == 0)
-                {
-                    // First we copy from the source array to the destination array
-                    length = ctrlByte1 & 3;
-                    LZCompliantCopy(ref sourceBytes, sourcePosition + 2, ref destinationBytes, destinationPosition, length);
+				if ((ctrlByte1 & 0x80) == 0) {
+					// First we copy from the source array to the destination array
+					length = ctrlByte1 & 3;
+					LZCompliantCopy(ref sourceBytes, sourcePosition + 2, ref destinationBytes, destinationPosition, length);
 
-                    // Then we copy characters already in the destination array to our current position in the destination array
-                    sourcePosition += length + 2;
-                    destinationPosition += length;
-                    length = ((ctrlByte1 & 0x1C) >> 2) + 3;
-                    offset = ((ctrlByte1 >> 5) << 8) + ctrlByte2 + 1;
-                    LZCompliantCopy(ref destinationBytes, destinationPosition - offset, ref destinationBytes, destinationPosition, length);
+					// Then we copy characters already in the destination array to our current position in the destination array
+					sourcePosition += length + 2;
+					destinationPosition += length;
+					length = ((ctrlByte1 & 0x1C) >> 2) + 3;
+					offset = ((ctrlByte1 >> 5) << 8) + ctrlByte2 + 1;
+					LZCompliantCopy(ref destinationBytes, destinationPosition - offset, ref destinationBytes, destinationPosition, length);
 
-                    destinationPosition += length;
-                }
+					destinationPosition += length;
+				}
 
 				// Control Characters 128 to 191 (3 byte length CC)
-				else if ((ctrlByte1 & 0x40) == 0)
-                {
-                    length = (ctrlByte2 >> 6) & 3;
-                    LZCompliantCopy(ref sourceBytes, sourcePosition + 3, ref destinationBytes, destinationPosition, length);
+				else if ((ctrlByte1 & 0x40) == 0) {
+					length = (ctrlByte2 >> 6) & 3;
+					LZCompliantCopy(ref sourceBytes, sourcePosition + 3, ref destinationBytes, destinationPosition, length);
 
-                    sourcePosition += length + 3;
-                    destinationPosition += length;
-                    length = (ctrlByte1 & 0x3F) + 4;
-                    offset = (ctrlByte2 & 0x3F) * 256 + ctrlByte3 + 1;
-                    LZCompliantCopy(ref destinationBytes, destinationPosition - offset, ref destinationBytes, destinationPosition, length);
+					sourcePosition += length + 3;
+					destinationPosition += length;
+					length = (ctrlByte1 & 0x3F) + 4;
+					offset = (ctrlByte2 & 0x3F) * 256 + ctrlByte3 + 1;
+					LZCompliantCopy(ref destinationBytes, destinationPosition - offset, ref destinationBytes, destinationPosition, length);
 
-                    destinationPosition += length;
-                }
+					destinationPosition += length;
+				}
 
 				// Control Characters 192 to 223 (4 byte length CC)
-				else if ((ctrlByte1 & 0x20) == 0)
-                {
-                    ctrlByte4 = sourceBytes[sourcePosition + 3];
+				else if ((ctrlByte1 & 0x20) == 0) {
+					ctrlByte4 = sourceBytes[sourcePosition + 3];
 
-                    length = ctrlByte1 & 3;
-                    LZCompliantCopy(ref sourceBytes, sourcePosition + 4, ref destinationBytes, destinationPosition, length);
+					length = ctrlByte1 & 3;
+					LZCompliantCopy(ref sourceBytes, sourcePosition + 4, ref destinationBytes, destinationPosition, length);
 
-                    sourcePosition += length + 4;
-                    destinationPosition += length;
-                    length = ((ctrlByte1 >> 2) & 3) * 256 + ctrlByte4 + 5;
-                    offset = ((ctrlByte1 & 0x10) << 12) + 256 * ctrlByte2 + ctrlByte3 + 1;
-                    LZCompliantCopy(ref destinationBytes, destinationPosition - offset, ref destinationBytes, destinationPosition, length);
+					sourcePosition += length + 4;
+					destinationPosition += length;
+					length = ((ctrlByte1 >> 2) & 3) * 256 + ctrlByte4 + 5;
+					offset = ((ctrlByte1 & 0x10) << 12) + 256 * ctrlByte2 + ctrlByte3 + 1;
+					LZCompliantCopy(ref destinationBytes, destinationPosition - offset, ref destinationBytes, destinationPosition, length);
 
-                    destinationPosition += length;
-                }
+					destinationPosition += length;
+				}
 
 				// Control Characters 224 to 251 (1 byte length CC)
 				else {
-                    length = (ctrlByte1 & 0x1F) * 4 + 4;
-                    LZCompliantCopy(ref sourceBytes, sourcePosition + 1, ref destinationBytes, destinationPosition, length);
+					length = (ctrlByte1 & 0x1F) * 4 + 4;
+					LZCompliantCopy(ref sourceBytes, sourcePosition + 1, ref destinationBytes, destinationPosition, length);
 
-                    sourcePosition += length + 1;
-                    destinationPosition += length;
-                }
-            }
+					sourcePosition += length + 1;
+					destinationPosition += length;
+				}
+			}
 
 			// Add trailing bytes
 			// Control Characters 252 to 255 (1 byte length CC)
-			if ((sourcePosition < sourceBytes.Length) && (destinationPosition < destinationBytes.Length))
-            {
-                LZCompliantCopy(ref sourceBytes, sourcePosition + 1, ref destinationBytes, destinationPosition, sourceBytes[sourcePosition] & 3);
-                destinationPosition += sourceBytes[sourcePosition] & 3;
-            }
+			if ((sourcePosition < sourceBytes.Length) && (destinationPosition < destinationBytes.Length)) {
+				LZCompliantCopy(ref sourceBytes, sourcePosition + 1, ref destinationBytes, destinationPosition, sourceBytes[sourcePosition] & 3);
+				destinationPosition += sourceBytes[sourcePosition] & 3;
+			}
 
-            if (destinationPosition != destinationBytes.Length)
-            {
-                //Logger.Log(LogLevel.Warning, "QFS bad length, {0} instead of {1}", destinationPosition, destinationBytes.Length);
-            }
+			if (destinationPosition != destinationBytes.Length) {
+				//Logger.Log(LogLevel.Warning, "QFS bad length, {0} instead of {1}", destinationPosition, destinationBytes.Length);
+			}
 
-            return destinationBytes;
+			return destinationBytes;
 		}
 
 
@@ -328,7 +312,7 @@ namespace QFS
 
 			public QFSCompress(byte[] data) {
 				dData = data;
-				cData = new byte[dData.Length-1];
+				cData = new byte[dData.Length - 1];
 
 				if (dData.Length < MaxWindowSize) {
 					windowSize = 1 << BitOperations.Log2((uint) dData.Length);
@@ -462,7 +446,7 @@ namespace QFS
 
 				Buffer.BlockCopy(cData, 0, temp, 4, cPos);
 				cData = temp;
-				
+
 
 				return cData;
 			}
@@ -687,7 +671,7 @@ namespace QFS
 
 			//these are the occurrence tables.
 			int[] rev_similar = new int[WINDOW_SIZE];    // indices are file position - auto initialized to zero
-			//rev_last stores length-distance pairs: aka "next *length* characters are equal to characters *distance* behind in cData
+														 //rev_last stores length-distance pairs: aka "next *length* characters are equal to characters *distance* behind in cData
 			int[,] rev_last = new int[256, 256];     // 64kB indices are successive input char bytes
 
 			int dPos;
@@ -698,7 +682,7 @@ namespace QFS
 
 			//5 byte header: 10FB then uncompressed file size
 			cData[0] = 0x10;
-			cData[1] = 0xFB;      
+			cData[1] = 0xFB;
 			cData[2] = (byte) ((dData.Length >> 16) & 0xff); //convert to big endian. only get last 8 bytes of the value
 			cData[3] = (byte) ((dData.Length >> 8) & 0xff);
 			cData[4] = (byte) (dData.Length & 0xff);
@@ -828,7 +812,7 @@ namespace QFS
 			}
 
 			Array.Resize(ref cData, cPos);     // finally
-			//Buflen = cPos; // length of the new compressed buffer
+											   //Buflen = cPos; // length of the new compressed buffer
 			return cData;
 		}
 
